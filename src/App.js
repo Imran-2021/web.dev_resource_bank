c5e55e5dfd36a7f3bd55f60b60b2650d972e47a9
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-09 23:49:20 +0600

    creating resource menu

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import {
 } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import NavBar from './Components/NavBar/NavBar';
+import Resources from "./Components/Resources/Resources";
 
 function App() {
   return (
       <Router>
            <NavBar/>
            <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Header/>
           </Route>
-          <Route path="/users">
-            {/* <Users /> */}
+          <Route path="/resources">
+           <Resources/>
           </Route>
           <Route path="/">
             {/* <Home /> */}
           </Route>
         </Switch>
+        <p style={{padding:"10px",background:"red",textAlign:"center"}}>end the game here. </p>
       </Router>
   );
 }
